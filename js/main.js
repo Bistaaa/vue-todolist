efbb110dd3fd2c3ba15bbd5dee9ad00f5d5873d5
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-22 17:18:53 +0200

    problema

--- js/main.js
+++ js/main.js
@@ -4,14 +4,23 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            newTask: ``,
+            tasks: [
+                { text: "Fare la spesa", done: false },
+                { text: "Pulire la camera", done: true },
+                { text: "Studiare per l'esame", done: false },
+                { text: "Chiamare l'idraulico", done: false }
+            ],
+          
         };
     },
+
     methods: {
-        askData() {
+        addTask() {
             
         }
     },
     
 }).mount("#app");
+
 
